b7031dc5a63dc35d580e94d4060e28f4edb43566
Shriram Rajaram <srajararam@kraususa.com>
2014-04-07 10:05:43 -0400

    Design

--- WayfairApp/WayfairApp/UI/WayFair.xaml.cs
+++ WayfairApp/WayfairApp/UI/WayFair.xaml.cs
@@ -10,19 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WayfairApp
+namespace WayfairApp.UI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WayFair.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WayFair : Window
     {
-        public MainWindow()
+        public WayFair()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //CanPonumber.Visibility = Visibility.Visible;
         }
     }
 }
